23f36ee90da9c4c18104a7d7bc0a2283c96e2440
Daniel Potts <drp777@hotmail.com>
2018-08-22 12:03:56 -0500

    created friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+		name: 'Rodney Dangerfield',
+		photo: 'https://goo.gl/images/EEG11T',
+		scores: [1, 5, 1, 1, 5, 5, 5, 1, 5, 5]
+	},{
+		name: 'Tiger Woods',
+		photo: 'https://goo.gl/images/qcjveY',
+		scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1]
+	},{
+		name: "Jennifer Lawrence,
+		photo: 'https://goo.gl/images/2eErSd',
+		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},{
+		name: 'Serena Williams',
+		photo: 'https://goo.gl/images/AynYD9',
+		scores: [5, 1, 5, 2, 1, 1, 1, 5, 1, 1]
+	}];
+
+module.exports = friends;
