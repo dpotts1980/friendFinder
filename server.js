189eb20d71433b17e9c66ef12a4ff6be91893f3b
Daniel Potts <drp777@hotmail.com>
2018-08-22 07:32:32 -0500

    built initial server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+
+// Dependencies
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.text());
+//app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+//Routing
+//=============================================================
+
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, || 8080, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
