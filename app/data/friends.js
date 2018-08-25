55aef6e9a113a72273571342627af3e768c4bdc9
Daniel Potts <drp777@hotmail.com>
2018-08-25 14:10:34 -0500

    final push, corrected images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,19 @@
 var friends = [
 	{
 		name: 'Rodney Dangerfield',
-		photo: 'https://goo.gl/images/EEG11T',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Rodney_Danagerfield_1972-1.jpg/220px-Rodney_Danagerfield_1972-1.jpg',
 		scores: [1, 5, 1, 1, 5, 5, 5, 1, 5, 5]
 	},{
 		name: 'Tiger Woods',
-		photo: 'https://goo.gl/images/qcjveY',
+		photo: 'https://sportshub.cbsistatic.com/i/2018/03/09/e31a15ac-1c20-44ee-aaf0-24ac35983033/tiger-woods-valspar-rd-1.jpg',
 		scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1]
 	},{
 		name: "Jennifer Lawrence",
-		photo: 'https://goo.gl/images/2eErSd',
+		photo: 'https://www.walldevil.com/wallpapers/a86/jennifer-lawrence-hot-girl-girls-wallpapers-kinopoisk-wallpaper.jpg',
 		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
 	},{
 		name: 'Serena Williams',
-		photo: 'https://goo.gl/images/AynYD9',
+		photo: 'https://media.kesq.com/npg-global-us-east-1/photo/2018/03/20/Serena%20Williams%20victory%20at%20Indian%20Wells.jpg.jpg_10757921_ver1.0_1280_720.jpg',
 		scores: [5, 1, 5, 2, 1, 1, 1, 5, 1, 1]
 	}];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,6 +11,6 @@ module.exports = function(app){
 	});
 
 	app.post('/api/friends', function(req, res){
-		friendList.push(req.body);
+		friendObject.push(req.body);
 	})
 };
